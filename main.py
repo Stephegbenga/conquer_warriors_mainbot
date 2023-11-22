bd98e3402dd2975f5981b76e312eba54733e616f
Stephegbenga <adegbamiyestephen2018@gmail.com>
2023-11-22 15:06:00 +0100

    pushed major update

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from flask import Flask, request
 app = Flask(__name__)
 from controllers.bot_engine import process_message
 
+
+@app.get("/")
+def get():
+    return {"status":"received"}
+
+
 @app.post("/webhook")
 def webhook():
     req = request.json
@@ -13,7 +19,6 @@ def webhook():
 def coinbase_webhook():
     req = request.json
     return {"status":"received"}
-
 
 if __name__ == '__main__':
     app.run()
